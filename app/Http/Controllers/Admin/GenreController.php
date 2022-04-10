d408ef0eaea1cbe701cd178949e978caa05c0643
Chagy <chagy_x_@hotmail.com>
2022-04-10 17:41:46 +0700

    Generate All Genres

--- app/Http/Controllers/Admin/GenreController.php
+++ app/Http/Controllers/Admin/GenreController.php
@@ -3,13 +3,49 @@
 namespace App\Http\Controllers\Admin;
 
 use Inertia\Inertia;
-use Illuminate\Http\Request;
+use App\Models\Genre;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class GenreController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Genres/Index');
+        $perPage = Request::input('perPage') ? : 5;
+
+        return Inertia::render('Genres/Index',[
+            'genres' => Genre::query()
+                            ->when(Request::input('search'),function($query,$search){
+                                $query->where('title','like',"%{$search}%");
+                            })
+                            ->paginate($perPage)
+                            ->withQueryString(),
+            'filters' => Request::only(['search','perPage'])
+        ]);
+    }
+
+    public function store()
+    {
+        $tmdb_genres = Http::get(config('services.tmdb.endpoint').'genre/movie/list?api_key='.config('services.tmdb.secret').'&language=en-US');
+        if($tmdb_genres->successful()){
+            $tmdb_genres_json = $tmdb_genres->json();
+            foreach ($tmdb_genres_json as $key => $single_tmdb_genre) {
+                foreach ($single_tmdb_genre as $key => $tgenre) {
+                    $genre = Genre::where('tmdb_id',$tgenre['id'])->first();
+                    if(!$genre){
+                        Genre::create([
+                            'tmdb_id' => $tgenre['id'],
+                            'title' => $tgenre['name']
+                        ]);
+                    }
+                    # code...
+                }
+            }
+
+            return Redirect::back()->with('flash.banner','genre created.');
+        }
+        return Redirect::back()->with('flash.banner','Api Error.');
     }
 }
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Genre extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tmdb_id',
+        'title',
+        'slug',
+    ];
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
