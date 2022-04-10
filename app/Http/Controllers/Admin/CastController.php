bdf8b98e192f09acf8c3785650dc6408ae663d3f
Chagy <chagy_x_@hotmail.com>
2022-04-10 11:35:32 +0700

    Cast Index Page

--- app/Http/Controllers/Admin/CastController.php
+++ app/Http/Controllers/Admin/CastController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Cast;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
 
 class CastController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Casts/Index');
+        return Inertia::render('Casts/Index',[
+            'casts' => Cast::paginate(5),
+            'filters' => Request::only(['search','perPage'])
+        ]);
     }
 }
