f1bcd2205261df24e22773d9f5c2f10134b31665
Chagy <chagy_x_@hotmail.com>
2022-04-10 20:08:02 +0700

    Episode Index Page

--- app/Http/Controllers/Admin/EpisodeController.php
+++ app/Http/Controllers/Admin/EpisodeController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers\Admin;
 
 use Inertia\Inertia;
-use Illuminate\Http\Request;
+use App\Models\Season;
+use App\Models\TvShow;
+use App\Models\Episode;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
 
 class EpisodeController extends Controller
 {
-    public function index()
+    public function index(TvShow $tvShow,Season $season)
     {
-        return Inertia::render('Casts/Index');
+        $perPage = Request::input('perPage') ? : 5;
+        
+        return Inertia::render('TvShows/Seasons/Episodes/Index',[
+            'tvShow' => $tvShow,
+            'episodes' => Episode::query()
+                            ->where('season_id',$season->id)
+                            ->when(Request::input('search'),function($query,$search){
+                                $query->where('name','like',"%{$search}%");
+                            })
+                            ->paginate($perPage)
+                            ->withQueryString(),
+            'filters' => Request::only(['search','perPage']),
+            'season' => $season
+        ]);
     }
 }
