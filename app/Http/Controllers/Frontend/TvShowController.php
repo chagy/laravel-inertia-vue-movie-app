cfbd6f96d889b08ae921c04ec6d438d43ad8f5b0
Chagy <chagy_x_@hotmail.com>
2022-04-17 20:06:38 +0700

    Tv Shows Index

--- app/Http/Controllers/Frontend/TvShowController.php
+++ app/Http/Controllers/Frontend/TvShowController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use Inertia\Inertia;
 use App\Models\Season;
 use App\Models\TvShow;
 use App\Models\Episode;
@@ -12,7 +13,9 @@ class TvShowController extends Controller
 {
     public function index()
     {
-        
+        return Inertia::render('Frontend/TvShows/Index',[
+            'tvShows' => TvShow::orderBy('created_at','desc')->paginate(12)
+        ]);
     }
 
     public function show(TvShow $tvShow)
